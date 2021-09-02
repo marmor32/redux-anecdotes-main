72ca8c89ed13413e5f0e30c060330ac38963a475
marmor32 <bhmi1994@gmail.com>
2021-09-02 08:10:56 +0300

    state debug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAction } from './reducers/anecdoteReducer'
+import { useState } from 'react'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
+  
+  const [debug, setDebug] = useState('debug')
 
-  let debug = { color : 'red' }
 
   const vote = (id) => {
     console.log('vote', id)
     dispatch(voteAction(id))
-    debug = { color : 'blue' }
+    setDebug('vote')
   }
 
   return (
     <div>
-      <h2 style={debug}>Anecdotes</h2>
+      <h2>Anecdotes {debug}</h2>
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
