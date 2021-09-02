4a5e811f56ccdea245f585cb594541a9ede9f35e
marmor32 <bhmi1994@gmail.com>
2021-09-02 15:39:11 +0300

    fixed }

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -44,12 +44,12 @@ export const voteAction = (id) => {
     type: 'VOTE',
     data: { id }
   })
+}
  export const newAnecdote = (content) => {
    return ({
      type: 'NEW_ANECDOTE',
      data: content
    })
  }
-}
 
 export default reducer
