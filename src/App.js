1d27a7c57bffc2ec9e82082ce8b337d645986ac7
marmor32 <bhmi1994@gmail.com>
2021-09-02 20:15:28 +0300

    AnecdoteList component

--- src/App.js
+++ src/App.js
@@ -5,38 +5,12 @@ import AnecdoteForm from './components/AnecdoteForm'
 
 
 const App = () => {
-    const anecdotes = useSelector(state => state)
-    const dispatch = useDispatch()
-    const votes = anecdotes.map(a => a.votes)
-
-
-    const vote = (id) => {
-      dispatch(voteAction(id))
-    }
-
-    const newEntry = (event) => {
-      event.preventDefault()
-      const content = event.target.entry.value
-      event.target.entry.value = ''
-      dispatch(newAnecdote(content))
-    }
-
 
     return (
       <div>
         <h2>Anecdotes</h2>
-        {anecdotes.map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
-          </div>
-        )}
-            <AnecdoteForm />
+        <AnecdoteList />
+        <AnecdoteForm />
       </div>
       )}
       
